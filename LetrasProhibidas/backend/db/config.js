f474739703c160359885a8fb9fe2313240134a11
Guille <alu0101505688@ull.edu.es>
2024-11-13 18:21:25 +0000

    conexion con base de datos

--- LetrasProhibidas/backend/db/config.js
+++ LetrasProhibidas/backend/db/config.js
@@ -1,8 +1,10 @@
 import { connect } from "mongoose";
 
+console.log(process.env.MONGODB_URI);
+
 connect((process.env.MONGODB_URI))
   .then(() => {
     console.log("Connected to MongoDB database")
-  }).catch(() => {
-    console.log("Unable to connect to MongoDB database")
-  })
+  }).catch((error) => {
+    console.log("Unable to connect to MongoDB database" + error)
+  });
