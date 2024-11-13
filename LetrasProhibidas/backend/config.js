5b6551900b3945290513042b85a5efa220d06b4b
Guille <alu0101505688@ull.edu.es>
2024-11-13 17:47:41 +0000

    connection to mongo database

--- LetrasProhibidas/backend/config.js
+++ LetrasProhibidas/backend/config.js
@@ -1,1 +1,8 @@
-// Aqui va la configuración de mongo
+import { connect } from "mongoose";
+
+connect((process.env.MONGODB_URI))
+  .then(() => {
+    console.log("Connected to MongoDB database")
+  }).catch(() => {
+    console.log("Unable to connect to MongoDB database")
+  })
