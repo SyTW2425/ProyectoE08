6f087faa45713698415b9b72dc1d59cf63429d08
OscarCordobesNavarro <alu0101478081@ull.edu.es>
2024-11-14 18:58:20 +0000

    Configuración a la base de datos completada

--- LetrasProhibidas/backend/db/config.js
+++ LetrasProhibidas/backend/db/config.js
@@ -1,14 +1,18 @@
 import { connect } from "mongoose";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-console.log(process.env.MONGODB_URI);
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/letrasprohibidas";
 
-connect(process.env.MONGODB_URI)
+if (!process.env.MONGODB_URI) {
+  console.log("No MONGODB_URI provided, using default URI");
+}
+
+connect(uri)
   .then(() => {
     console.log("Connected to MongoDB database");
   })
   .catch((error) => {
-    console.log("Unable to connect to MongoDB database" + error);
+    console.log("Unable to connect to MongoDB database");
+    console.log(error);
   });
