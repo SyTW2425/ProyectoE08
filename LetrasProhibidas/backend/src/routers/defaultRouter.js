e379c0b6dec143a7d1e65310d34dfb68756e5b94
Guille <alu0101505688@ull.edu.es>
2024-11-15 18:58:55 +0000

    El router por defecto sirve el frontend

--- LetrasProhibidas/backend/src/routers/defaultRouter.js
+++ LetrasProhibidas/backend/src/routers/defaultRouter.js
@@ -1,6 +1,17 @@
 import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
 
 export const defaultRouter = express.Router();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+defaultRouter.use(express.static(path.join(__dirname, "../../../frontend/build")));
+
+defaultRouter.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../../frontend/build", "index.html"));
+});
 
 defaultRouter.all("*", (_, res) => {
   res.status(501).send("Not implemented");
