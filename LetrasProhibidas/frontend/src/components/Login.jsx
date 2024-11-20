756e5aad0803b71f10a1a899f9be5ad5c6914f35
Guille <alu0101505688@ull.edu.es>
2024-11-20 20:45:19 +0000

    empezado login

--- LetrasProhibidas/frontend/src/components/Login.jsx
+++ LetrasProhibidas/frontend/src/components/Login.jsx
@@ -3,7 +3,7 @@ import { Background } from "./Background"
 export const Login = () => {
   return(
     <Background>
-      <div className="h-[40rem] w-[60rem] border-8">
+      <div className="h-[40rem] w-[60rem] border-[10px] rounded-xl border-white/10 backdrop-blur-xl">
         Esto es el login
       </div>
     </Background>
