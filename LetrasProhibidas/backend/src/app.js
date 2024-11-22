26959906754bbbf6c9520ddd53b49bffd575b069
OscarCordobesNavarro <alu0101478081@ull.edu.es>
2024-11-22 18:05:10 +0000

    Cambios en el cors

--- LetrasProhibidas/backend/src/app.js
+++ LetrasProhibidas/backend/src/app.js
@@ -6,8 +6,14 @@ import { gameRouter } from "./routers/gameRouter.js";
 import { userRouter } from "./routers/userRouter.js";
 import { lobbyRouter } from "./routers/lobbyRouter.js";
 import { defaultRouter } from "./routers/defaultRouter.js";
+import cors from "cors";
 
 export const app = express();
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+}));
 
 app.use(express.json());
 
@@ -15,6 +21,7 @@ app.use(userRouter);
 app.use(gameRouter);
 app.use(lobbyRouter);
 app.use(defaultRouter);
+
 
 export const httpServer = createServer(app);
 export const io = new Server(httpServer, {
