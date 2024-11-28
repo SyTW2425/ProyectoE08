1ed4562ee4a4bbf5555ae3791c62050da0583687
Guille <alu0101505688@ull.edu.es>
2024-11-28 11:51:27 +0000

    esqueleto del bento

--- LetrasProhibidas/frontend/src/components/pages/Home.jsx
+++ LetrasProhibidas/frontend/src/components/pages/Home.jsx
@@ -36,10 +36,43 @@ export const Home = ({onLogout}) => {
       <div>
         {
           userData ? 
-          <div>
-            {JSON.stringify(userData)}
-            <button onClick={() => onLogout()}>Logout</button>
-          </div>
+            <div className="h-[40rem] w-[60rem] border-[10px] p-5 rounded-xl border-white/10 backdrop-blur-xl flex flex-col items-center justify-start shadow-xl font-poppins text-white">
+              <h1 className="text-[64px] font-black"><span className="text-white">LETRAS</span> <span className="bg-gradient-to-l from-primaryBlue from-70% to-[#8ee5ff] bg-clip-text text-transparent">PROHIBIDAS</span></h1>
+              <div className="h-full w-[50rem] flex flex-row gap-4 items-center justify-start p-3">
+                {/* Aqui va el bento */}
+                
+                <div className="flex h-full w-full items-center justify-center">
+                  <div className="grid h-full w-full gap-4 bg-gray-200 p-2 grid-cols-5 grid-rows-4 rounded-lg shadow-md">
+                  
+                    <div 
+                      className="col-span-2 row-span-2 bg-pink-200 rounded-lg shadow-md flex items-center justify-center"
+                    >
+                      <p>Salmon</p>
+                    </div>
+                  
+                    <div 
+                      className="col-span-3 row-span-2 bg-lime-200 rounded-lg shadow-md flex items-center justify-center"
+                    >
+                      <p>Broccoli</p>
+                    </div>
+                  
+                    <div 
+                      className="col-span-3 row-span-2 bg-yellow-200 rounded-lg shadow-md flex items-center justify-center"
+                    >
+                      <p>Tamago</p>
+                    </div>
+                  
+                    <div 
+                      className="col-span-2 row-span-2 bg-tan-200 rounded-lg shadow-md flex items-center justify-center"
+                    >
+                      <p>Pork</p>
+                    </div>
+                  
+                  </div>
+                </div>
+
+              </div>
+            </div>
           : 
           <p>"Cargando.."</p>
         }
