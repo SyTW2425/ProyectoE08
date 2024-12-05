ab20ca55f43b24931adf7ad56cdabf7dd209b1b8
Guillermo Díaz <gdiazbricio@gmail.com>
2024-12-05 16:42:11 +0000

    terminado menu principal

--- LetrasProhibidas/frontend/src/components/assets/BentoItem.jsx
+++ LetrasProhibidas/frontend/src/components/assets/BentoItem.jsx
@@ -1,8 +1,8 @@
 export const BentoItem = ({title, description, background}) => {
   return (
-    <button style={{ backgroundImage: `url(${background})` }} className="bg-cover bg-center w-full h-full rounded-lg flex flex-col justify-end border-[4px] border-white">
+    <button style={{ backgroundImage: `url(${background})` }} className="bg-cover bg-center w-full h-full rounded-lg flex flex-col justify-end border-[4px] grayscale hover:grayscale-0 transition duration-150 ease-in-out border-white">
       <div className="p-2 bg-gradient-to-t from-black to-transparent rounded-lg w-full text-left">
-        <h1 className="font-black text-3xl">{title}</h1>
+        <h1 className="font-black text-3xl drop-shadow-md">{title}</h1>
         <p className="line-clamp-2 text-lg">{description}</p>
       </div>
     </button>
--- LetrasProhibidas/frontend/src/components/pages/Home.jsx
+++ LetrasProhibidas/frontend/src/components/pages/Home.jsx
@@ -40,38 +40,22 @@ export const Home = ({ onLogout }) => {
             <div className="h-[40rem] w-[60rem] border-[10px] p-5 rounded-xl border-white/10 backdrop-blur-xl flex flex-col items-center justify-start shadow-xl font-poppins text-white">
               <h1 className="text-[64px] font-black"><span className="text-white">LETRAS</span> <span className="bg-gradient-to-l from-primaryBlue from-70% to-[#8ee5ff] bg-clip-text text-transparent">PROHIBIDAS</span></h1>
               <div className="h-full w-[50rem] flex flex-row gap-4 items-center justify-start p-3">
-                {/* Aqui va el bento */}
-
                 <div className="flex h-full w-full items-center justify-center">
                   <div className="grid h-full w-full gap-4 p-2 grid-cols-5 grid-rows-4 rounded-lg shadow-md">
-
-                    <div
-                      className="col-span-2 row-span-2 rounded-lg shadow-md flex items-center justify-center"
-                    >
-                      <BentoItem title="PERFIL" description="Echa un vistazo a tus partidas ganadas, estadísticas y logros." background="bento/profile.png"/>
+                    <div className="col-span-2 row-span-2 rounded-lg shadow-md flex items-center justify-center">
+                      <BentoItem title="PERFIL" description="Echa un vistazo a tus partidas ganadas, estadísticas y logros." background="bento/profile.webp"/>
                     </div>
-
-                    <div
-                      className="col-span-3 row-span-2 rounded-lg shadow-md flex items-center justify-center"
-                    >
-                      <BentoItem title="JUGAR" description="¿No tienes con quién jugar? No hay problema, busca una partida y conoce gente nueva." background="bento/play.png"/>
+                    <div className="col-span-3 row-span-2 rounded-lg shadow-md flex items-center justify-center">
+                      <BentoItem title="JUGAR" description="¿No tienes con quién jugar? No hay problema, busca una partida y conoce gente nueva." background="bento/play.webp"/>
                     </div>
-
-                    <div
-                      className="col-span-3 row-span-2 rounded-lg shadow-md flex items-center justify-center"
-                    >
-                      <BentoItem title="UNIRSE" description="Pide a tus amigos el código de la sala y juega junto a ellos." background="bento/join.png"/>
+                    <div className="col-span-3 row-span-2 rounded-lg shadow-md flex items-center justify-center">
+                      <BentoItem title="UNIRSE" description="Pide a tus amigos el código de la sala y juega junto a ellos." background="bento/join.webp"/>
                     </div>
-
-                    <div
-                      className="col-span-2 row-span-2 rounded-lg shadow-md flex items-center justify-center"
-                    >
-                      <BentoItem title="CREAR SALA" description="Crea una sala con tus propias reglas, ¡no te olvides de invitar a tus amigos!" background="bento/create.png"/>
+                    <div className="col-span-2 row-span-2 rounded-lg shadow-md flex items-center justify-center">
+                      <BentoItem title="CREAR SALA" description="Crea una sala con tus propias reglas, ¡no te olvides de invitar a tus amigos!" background="bento/create.webp"/>
                     </div>
-
                   </div>
                 </div>
-
               </div>
             </div>
             :
