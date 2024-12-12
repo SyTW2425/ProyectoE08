d5350905da42496b640a8308b25020e3a11f0ca6
Guillermo Díaz <gdiazbricio@gmail.com>
2024-12-12 18:04:26 +0000

    perfil casi hecho

--- LetrasProhibidas/frontend/src/components/pages/Profile.jsx
+++ LetrasProhibidas/frontend/src/components/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { StatCard } from "../assets/StatCard"
 
 export const Profile = () => {
   const [userData, setUserData] = useState()
@@ -34,10 +35,24 @@ export const Profile = () => {
     <div>
       {userData ? 
         <div>
-          <p>{userData.name}</p>
-          <p>Partidas jugadas: {userData.gamesPlayed}</p>
-          <p>Palabras acertadas:{userData.wordsGuessed}</p>
-          <p>Partidas ganadas:{userData.gamesWon}</p>
+          <div>
+            <div className="h-[40rem] w-[60rem] border-[10px] p-5 rounded-xl border-white/10 backdrop-blur-xl flex flex-col items-center justify-start shadow-xl font-poppins text-white">
+              <h1 className="text-[64px] font-black"><span className="text-white">¡HOLA</span> <span className="bg-gradient-to-l from-primaryBlue from-70% to-[#8ee5ff] bg-clip-text text-transparent">{userData.name.toUpperCase()}</span>!</h1>
+              <div className="h-full w-[50rem] flex flex-row gap-4 items-center justify-start p-3">
+                <div className="flex h-full w-full items-center flex-col gap-10">
+                  <img
+                    src={userData.avatarSrc}
+                    className="rounded-full border-white border-[7px] max-w-56 max-h-56"
+                  />
+                  <div className="flex items-center gap-4">
+                    <StatCard title="partidas jugadas" number={userData.gamesPlayed}/>
+                    <StatCard title="partidas ganadas" number={userData.gamesWon}/>
+                    <StatCard title="palabras acertadas" number={userData.wordsGuessed}/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         :
         "cargando"
