b96525a315b4c6fd214d8dad5433b9927520d1b4
OscarCordobesNavarro <alu0101478081@ull.edu.es>
2024-12-21 21:52:25 +0000

    config de la base de datos en la appTest

--- LetrasProhibidas/backend/src/appTest.js
+++ LetrasProhibidas/backend/src/appTest.js
@@ -3,6 +3,7 @@ import { gameRouter } from "./routers/gameRouter.js";
 import { userRouter } from "./routers/userRouter.js";
 import { lobbyRouter } from "./routers/lobbyRouter.js";
 import { defaultRouter } from "./routers/defaultRouter.js";
+import "./db/configTest.js";
 
 export const app = express();
 
