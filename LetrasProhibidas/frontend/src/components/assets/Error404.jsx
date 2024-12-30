c6c4fede996780fe8491b073e17cf609979e60ff
OscarCordobesNavarro <alu0101478081@ull.edu.es>
2024-12-30 00:36:24 +0000

    Update: Texto en blanco en pantalla de error

--- LetrasProhibidas/frontend/src/components/assets/Error404.jsx
+++ LetrasProhibidas/frontend/src/components/assets/Error404.jsx
@@ -19,7 +19,9 @@ export const Error404 = () => {
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                 </div>
-                <StandardButton text="Inicio" onClick={() => navigate('/')} />
+                <div className='text-white'>
+                    <StandardButton text="Inicio" onClick={() => navigate('/')} />
+                </div>
             </div>
         </div>
     );
